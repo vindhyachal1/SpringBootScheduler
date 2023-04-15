d4310dccd57e78fc05b993f53cfffa3df6035d02
Vindhyachal Tiwari <vindhyachaltiwari01@gmail.com>
2023-04-15 17:36:25 +0530

    first

---
+++ src/main/java/com/example/demo/ScheduledTasks.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+    @Autowired
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ScheduledTasks(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    @Scheduled(cron = "* * * * * *")
+    public void doTask() {
+        String sql = "INSERT INTO first.tutorial_tbl(tutorial_title, tutorial_author, submission_date) " +
+                "VALUES('Title2', 'T', '2008-11-12')";
+        jdbcTemplate.update(sql);
+    }
+}
