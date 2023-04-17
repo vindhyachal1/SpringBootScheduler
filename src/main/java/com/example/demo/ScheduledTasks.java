23ad410bb310d03aab55d5f803107c253cf6398b
Vindhyachal Tiwari <vindhyachaltiwari01@gmail.com>
2023-04-17 10:49:49 +0530

    first

--- src/main/java/com/example/demo/ScheduledTasks.java
+++ src/main/java/com/example/demo/ScheduledTasks.java
@@ -5,6 +5,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class ScheduledTasks {
 
@@ -18,11 +21,18 @@ public class ScheduledTasks {
 
     @Scheduled(cron = "* * * * * *")
     public void doTask() {
-        String sql = "INSERT INTO first.tutorial_tbl(tutorial_title, tutorial_author, submission_date) " +
-                "VALUES('Title4', 'C', '2008-11-12')";
-        String sql1 = "INSERT INTO first.tutorial_tbl(tutorial_title, tutorial_author, submission_date) " +
-                "VALUES('Title5', 'D', '2008-11-12')";
-        jdbcTemplate.update(sql);
-        jdbcTemplate.update(sql1);
+        List<String> sqlQueries = new ArrayList<>();
+//        String[] sqlQueries = new String[100];
+        sqlQueries.add ("INSERT INTO first.tutorial_tbl(tutorial_title, tutorial_author, submission_date) " +
+                "VALUES('Title4', 'C', '2008-11-12')");
+        sqlQueries.add ("INSERT INTO first.tutorial_tbl(tutorial_title, tutorial_author, submission_date) " +
+                "VALUES('Title5', 'D', '2008-11-12')");
+
+        // loop through the array and execute each SQL query
+        for (String sql : sqlQueries) {
+            jdbcTemplate.update(sql);
+        }
+//        jdbcTemplate.update(sql);
+//        jdbcTemplate.update(sql1);
     }
 }
